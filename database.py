1ea1b1973755b43761eaa64cd2d78d22ce5a21b0
olabode omoyele <olabode.omoyele@helloworlpc.com>
2025-01-24 13:32:51 +0100

    feat: use external db

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Define the database URL (update with your actual database URL)
-DATABASE_URL = "postgresql+asyncpg://user:password@db/citizen_ai"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the SQLAlchemy async engine
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -24,3 +26,4 @@ async def get_db():
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
